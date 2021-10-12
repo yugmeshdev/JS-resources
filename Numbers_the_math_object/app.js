b702c1476133e5e7ae4f8b7bae1c1afdfcfb8e76
unknown <yugmeshk2139@gmail.com>
2021-10-12 16:22:25 +0530

    lec 0009

---
+++ Numbers_the_math_object/app.js
@@ -0,0 +1,18 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+val = num1 + num2;
+
+
+
+
+//Math Object
+val=Math.PI;
+val=Math.E;
+val=Math.round(2.8);
+val=Math.abs(-3);
+val=Math.min(2,32);
+
+
+
+console.log(val);
